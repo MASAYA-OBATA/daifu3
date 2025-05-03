e7f46c91a02e9a72032a8f312b082e1a795fbe24
MASAYA-OBATA <yumemipo@yahoo.co.jp>
2025-05-03 21:00:27 +0900

    Delete GameEndSummry.kt

--- GameEndSummry.kt
+++
@@ -1,8 +0,0 @@
-package com.example.daifu3.data
-
-data class GameEndSummary(
-    val winnerName: String,
-    val scoreChanges: Map<String, Int>,
-    val finalScores: Map<String, Int>,
-    val finalRanks: Map<String, PlayerRank>
-)
